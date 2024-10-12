3556534c8c4fc91b36ed11279ab321306724b684
Joel Rebello <mail.joelr@gmail.com>
2024-10-12 14:39:23 +0200

    db/migrations: consolidate schema into a single migration file
    This is done to enable updating the schema for the postgres switch

--- db/migrations/00001_init.sql
+++ db/migrations/00001_init.sql
@@ -9,8 +9,7 @@ CREATE TABLE public.servers (
     updated_at TIMESTAMPTZ NULL,
     deleted_at TIMESTAMPTZ NULL,
     CONSTRAINT "primary" PRIMARY KEY (id ASC),
-    INDEX idx_facility (facility_code ASC),
-    FAMILY "primary" (id, name, facility_code, created_at, updated_at, deleted_at)
+    INDEX idx_facility (facility_code ASC)
 );
 
 CREATE TABLE public.server_component_types (
@@ -21,8 +20,7 @@ CREATE TABLE public.server_component_types (
     slug STRING NOT NULL,
     CONSTRAINT "primary" PRIMARY KEY (id ASC),
     UNIQUE INDEX idx_name (name ASC),
-    UNIQUE INDEX server_component_types_slug_key (slug ASC),
-    FAMILY "primary" (id, name, created_at, updated_at, slug)
+    UNIQUE INDEX server_component_types_slug_key (slug ASC)
 );
 
 CREATE TABLE public.server_components (
@@ -39,7 +37,8 @@ CREATE TABLE public.server_components (
     INDEX idx_server_component_type_id (server_component_type_id ASC),
     INDEX idx_server_id (server_id ASC),
     UNIQUE INDEX idx_server_components (server_id ASC, serial ASC, server_component_type_id ASC) WHERE ((server_id IS NOT NULL) AND (serial IS NOT NULL)) AND (server_component_type_id IS NOT NULL),
-    FAMILY "primary" (id, name, vendor, model, serial, server_component_type_id, server_id, created_at, updated_at)
+    CONSTRAINT fk_server_component_type_id_ref_server_component_types FOREIGN KEY (server_component_type_id) REFERENCES public.server_component_types(id),
+    CONSTRAINT fk_server_id_ref_servers FOREIGN KEY (server_id) REFERENCES public.servers(id) ON DELETE CASCADE
 );
 
 CREATE TABLE public.versioned_attributes (
@@ -49,23 +48,18 @@ CREATE TABLE public.versioned_attributes (
     data JSONB NOT NULL,
     created_at TIMESTAMPTZ NULL,
     updated_at TIMESTAMPTZ NULL,
-    -- ensure exactly one relationship is set
-    CHECK (
-        (
-            (server_id is not null)::integer +
-            (server_component_id is not null)::integer
-        ) = 1
-    ),
     server_component_id UUID NULL,
     tally INT8 NOT NULL DEFAULT 0:::INT8,
     CONSTRAINT "primary" PRIMARY KEY (id ASC),
     INDEX idx_server_id (server_id ASC) WHERE server_id IS NOT NULL,
     INDEX idx_server_namespace (server_id ASC, namespace ASC, created_at ASC) WHERE server_id IS NOT NULL,
-    INVERTED INDEX idx_server_data (server_id, namespace, data) WHERE server_id IS NOT NULL,
+    INVERTED INDEX idx_server_data (server_id ASC, namespace ASC, data) WHERE server_id IS NOT NULL,
     INDEX idx_server_component_id (server_component_id ASC) WHERE server_component_id IS NOT NULL,
     INDEX idx_server_component_namespace (server_component_id ASC, namespace ASC, created_at ASC) WHERE server_component_id IS NOT NULL,
-    INVERTED INDEX idx_server_component_data (server_component_id, namespace, data) WHERE server_component_id IS NOT NULL,
-    FAMILY "primary" (id, server_id, namespace, data, created_at, updated_at, server_component_id, tally)
+    INVERTED INDEX idx_server_component_data (server_component_id ASC, namespace ASC, data) WHERE server_component_id IS NOT NULL,
+    CONSTRAINT check_server_id_server_component_id CHECK (((server_id IS NOT NULL)::INT8 + (server_component_id IS NOT NULL)::INT8) = 1:::INT8),
+    CONSTRAINT fk_server_id_ref_servers FOREIGN KEY (server_id) REFERENCES public.servers(id) ON DELETE CASCADE,
+    CONSTRAINT fk_server_component_id_ref_server_components FOREIGN KEY (server_component_id) REFERENCES public.server_components(id) ON DELETE CASCADE
 );
 
 CREATE TABLE public.attributes (
@@ -76,21 +70,16 @@ CREATE TABLE public.attributes (
     data JSONB NOT NULL,
     created_at TIMESTAMPTZ NULL,
     updated_at TIMESTAMPTZ NULL,
-    -- ensure exactly one relationship is set
-    CHECK (
-        (
-            (server_id is not null)::integer +
-            (server_component_id is not null)::integer
-        ) = 1
-    ),
     CONSTRAINT "primary" PRIMARY KEY (id ASC),
     INDEX idx_server_id (server_id ASC) WHERE server_id IS NOT NULL,
     UNIQUE INDEX idx_server_namespace (server_id ASC, namespace ASC) WHERE server_id IS NOT NULL,
-    INVERTED INDEX idx_server_data (server_id, namespace, data) WHERE server_id IS NOT NULL,
+    INVERTED INDEX idx_server_data (server_id ASC, namespace ASC, data) WHERE server_id IS NOT NULL,
     INDEX idx_server_component_id (server_component_id ASC) WHERE server_component_id IS NOT NULL,
     UNIQUE INDEX idx_server_component_namespace (server_component_id ASC, namespace ASC) WHERE server_component_id IS NOT NULL,
-    INVERTED INDEX idx_server_component_data (server_component_id, namespace, data) WHERE server_component_id IS NOT NULL,
-    FAMILY "primary" (id, server_id, server_component_id, namespace, data, created_at, updated_at)
+    INVERTED INDEX idx_server_component_data (server_component_id ASC, namespace ASC, data) WHERE server_component_id IS NOT NULL,
+    CONSTRAINT check_server_id_server_component_id CHECK (((server_id IS NOT NULL)::INT8 + (server_component_id IS NOT NULL)::INT8) = 1:::INT8),
+    CONSTRAINT fk_server_id_ref_servers FOREIGN KEY (server_id) REFERENCES public.servers(id) ON DELETE CASCADE,
+    CONSTRAINT fk_server_component_id_ref_server_components FOREIGN KEY (server_component_id) REFERENCES public.server_components(id) ON DELETE CASCADE
 );
 
 CREATE TABLE public.component_firmware_version (
@@ -105,9 +94,10 @@ CREATE TABLE public.component_firmware_version (
     repository_url STRING NOT NULL,
     created_at TIMESTAMPTZ NULL,
     updated_at TIMESTAMPTZ NULL,
+    install_inband BOOL NOT NULL DEFAULT false,
+    oem BOOL NOT NULL DEFAULT false,
     CONSTRAINT "primary" PRIMARY KEY (id ASC),
-    UNIQUE INDEX vendor_component_version_filename_unique (vendor ASC, component ASC, version ASC, filename ASC),
-    FAMILY "primary" (id, component, vendor, model, filename, version, checksum, upstream_url, repository_url, created_at, updated_at)
+    UNIQUE INDEX vendor_component_version_filename_unique (vendor ASC, component ASC, version ASC, filename ASC)
 );
 
 CREATE TABLE public.server_credential_types (
@@ -118,8 +108,7 @@ CREATE TABLE public.server_credential_types (
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL,
     CONSTRAINT "primary" PRIMARY KEY (id ASC),
-    UNIQUE INDEX server_secret_types_slug_key (slug ASC),
-    FAMILY "primary" (id, name, slug, builtin, created_at, updated_at)
+    UNIQUE INDEX server_secret_types_slug_key (slug ASC)
 );
 
 CREATE TABLE public.server_credentials (
@@ -132,7 +121,8 @@ CREATE TABLE public.server_credentials (
     username STRING NOT NULL,
     CONSTRAINT "primary" PRIMARY KEY (id ASC),
     UNIQUE INDEX idx_server_secrets_by_type (server_id ASC, server_credential_type_id ASC),
-    FAMILY "primary" (id, server_id, server_credential_type_id, password, created_at, updated_at, username)
+    CONSTRAINT fk_server_id_ref_servers FOREIGN KEY (server_id) REFERENCES public.servers(id) ON DELETE CASCADE,
+    CONSTRAINT fk_server_secret_type_id_ref_server_secret_types FOREIGN KEY (server_credential_type_id) REFERENCES public.server_credential_types(id)
 );
 
 CREATE TABLE public.component_firmware_set (
@@ -141,8 +131,7 @@ CREATE TABLE public.component_firmware_set (
     created_at TIMESTAMPTZ NULL,
     updated_at TIMESTAMPTZ NULL,
     CONSTRAINT "primary" PRIMARY KEY (id ASC),
-    UNIQUE INDEX idx_name (name ASC),
-    FAMILY "primary" (id, name, created_at, updated_at)
+    UNIQUE INDEX idx_name (name ASC)
 );
 
 CREATE TABLE public.component_firmware_set_map (
@@ -151,7 +140,8 @@ CREATE TABLE public.component_firmware_set_map (
     firmware_id UUID NOT NULL,
     CONSTRAINT "primary" PRIMARY KEY (id ASC),
     UNIQUE INDEX component_firmware_set_map_firmware_set_id_firmware_id_key (firmware_set_id ASC, firmware_id ASC),
-    FAMILY "primary" (id, firmware_set_id, firmware_id)
+    CONSTRAINT fk_firmware_set_id_ref_component_firmware_set FOREIGN KEY (firmware_set_id) REFERENCES public.component_firmware_set(id) ON DELETE CASCADE,
+    CONSTRAINT fk_firmware_id_ref_component_firmware_version FOREIGN KEY (firmware_id) REFERENCES public.component_firmware_version(id) ON DELETE RESTRICT
 );
 
 CREATE TABLE public.attributes_firmware_set (
@@ -163,9 +153,9 @@ CREATE TABLE public.attributes_firmware_set (
     updated_at TIMESTAMPTZ NULL,
     CONSTRAINT "primary" PRIMARY KEY (id ASC),
     INDEX idx_firmware_set_id (firmware_set_id ASC) WHERE firmware_set_id IS NOT NULL,
-    INVERTED INDEX idx_firmware_set_data (firmware_set_id, namespace, data) WHERE firmware_set_id IS NOT NULL,
+    INVERTED INDEX idx_firmware_set_data (firmware_set_id ASC, namespace ASC, data) WHERE firmware_set_id IS NOT NULL,
     UNIQUE INDEX idx_firmware_set_namespace (firmware_set_id ASC, namespace ASC) WHERE firmware_set_id IS NOT NULL,
-    FAMILY "primary" (id, firmware_set_id, namespace, data, created_at, updated_at)
+    CONSTRAINT fk_firmware_set_id_ref_component_firmware_set FOREIGN KEY (firmware_set_id) REFERENCES public.component_firmware_set(id) ON DELETE CASCADE
 );
 
 CREATE TABLE public.bom_info (
@@ -175,40 +165,102 @@ CREATE TABLE public.bom_info (
     num_defi_pmi STRING NULL,
     num_def_pwd STRING NULL,
     metro STRING NULL,
-    CONSTRAINT "primary" PRIMARY KEY (serial_num ASC),
-    FAMILY "primary" (serial_num, aoc_mac_address, bmc_mac_address, num_defi_pmi, num_def_pwd, metro)
+    CONSTRAINT "primary" PRIMARY KEY (serial_num ASC)
 );
 
 CREATE TABLE public.aoc_mac_address (
     aoc_mac_address STRING NOT NULL,
     serial_num STRING NOT NULL,
     CONSTRAINT "primary" PRIMARY KEY (aoc_mac_address ASC),
-    FAMILY "primary" (aoc_mac_address, serial_num)
+    CONSTRAINT fk_serial_num_ref_bom_info FOREIGN KEY (serial_num) REFERENCES public.bom_info(serial_num) ON DELETE CASCADE
 );
 
 CREATE TABLE public.bmc_mac_address (
     bmc_mac_address STRING NOT NULL,
     serial_num STRING NOT NULL,
     CONSTRAINT "primary" PRIMARY KEY (bmc_mac_address ASC),
-    FAMILY "primary" (bmc_mac_address, serial_num)
+    CONSTRAINT fk_serial_num_ref_bom_info FOREIGN KEY (serial_num) REFERENCES public.bom_info(serial_num) ON DELETE CASCADE
 );
 
-INSERT INTO server_credential_types(name, slug, builtin, created_at, updated_at)
-  VALUES ('BMC', 'bmc', true, now(), now());
+CREATE TABLE public.bios_config_sets (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    name STRING NOT NULL,
+    version STRING NOT NULL,
+    created_at TIMESTAMPTZ NULL,
+    updated_at TIMESTAMPTZ NULL,
+    CONSTRAINT bios_config_sets_pkey PRIMARY KEY (id ASC),
+    UNIQUE INDEX bios_config_sets_name_key (name ASC)
+);
 
-ALTER TABLE public.server_components ADD CONSTRAINT fk_server_component_type_id_ref_server_component_types FOREIGN KEY (server_component_type_id) REFERENCES public.server_component_types(id);
-ALTER TABLE public.server_components ADD CONSTRAINT fk_server_id_ref_servers FOREIGN KEY (server_id) REFERENCES public.servers(id) ON DELETE CASCADE;
-ALTER TABLE public.versioned_attributes ADD CONSTRAINT fk_server_id_ref_servers FOREIGN KEY (server_id) REFERENCES public.servers(id) ON DELETE CASCADE;
-ALTER TABLE public.versioned_attributes ADD CONSTRAINT fk_server_component_id_ref_server_components FOREIGN KEY (server_component_id) REFERENCES public.server_components(id) ON DELETE CASCADE;
-ALTER TABLE public.attributes ADD CONSTRAINT fk_server_id_ref_servers FOREIGN KEY (server_id) REFERENCES public.servers(id) ON DELETE CASCADE;
-ALTER TABLE public.attributes ADD CONSTRAINT fk_server_component_id_ref_server_components FOREIGN KEY (server_component_id) REFERENCES public.server_components(id) ON DELETE CASCADE;
-ALTER TABLE public.server_credentials ADD CONSTRAINT fk_server_id_ref_servers FOREIGN KEY (server_id) REFERENCES public.servers(id) ON DELETE CASCADE;
-ALTER TABLE public.server_credentials ADD CONSTRAINT fk_server_secret_type_id_ref_server_secret_types FOREIGN KEY (server_credential_type_id) REFERENCES public.server_credential_types(id);
-ALTER TABLE public.component_firmware_set_map ADD CONSTRAINT fk_firmware_set_id_ref_component_firmware_set FOREIGN KEY (firmware_set_id) REFERENCES public.component_firmware_set(id) ON DELETE CASCADE;
-ALTER TABLE public.component_firmware_set_map ADD CONSTRAINT fk_firmware_id_ref_component_firmware_version FOREIGN KEY (firmware_id) REFERENCES public.component_firmware_version(id) ON DELETE RESTRICT;
-ALTER TABLE public.attributes_firmware_set ADD CONSTRAINT fk_firmware_set_id_ref_component_firmware_set FOREIGN KEY (firmware_set_id) REFERENCES public.component_firmware_set(id) ON DELETE CASCADE;
-ALTER TABLE public.aoc_mac_address ADD CONSTRAINT fk_serial_num_ref_bom_info FOREIGN KEY (serial_num) REFERENCES public.bom_info(serial_num) ON DELETE CASCADE;
-ALTER TABLE public.bmc_mac_address ADD CONSTRAINT fk_serial_num_ref_bom_info FOREIGN KEY (serial_num) REFERENCES public.bom_info(serial_num) ON DELETE CASCADE;
+CREATE TABLE public.bios_config_components (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    fk_bios_config_set_id UUID NOT NULL,
+    name STRING NOT NULL,
+    vendor STRING NOT NULL,
+    model STRING NOT NULL,
+    created_at TIMESTAMPTZ NULL,
+    updated_at TIMESTAMPTZ NULL,
+    CONSTRAINT bios_config_components_pkey PRIMARY KEY (id ASC),
+    UNIQUE INDEX bios_config_components_fk_bios_config_set_id_name_key (fk_bios_config_set_id ASC, name ASC),
+    CONSTRAINT bios_config_components_fk_bios_config_set_id_fkey FOREIGN KEY (fk_bios_config_set_id) REFERENCES public.bios_config_sets(id) ON DELETE CASCADE
+);
 
+CREATE TABLE public.bios_config_settings (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    fk_bios_config_component_id UUID NOT NULL,
+    settings_key STRING NOT NULL,
+    settings_value STRING NOT NULL,
+    raw JSONB NULL,
+    created_at TIMESTAMPTZ NULL,
+    updated_at TIMESTAMPTZ NULL,
+    CONSTRAINT bios_config_settings_pkey PRIMARY KEY (id ASC),
+    UNIQUE INDEX bios_config_settings_fk_bios_config_component_id_settings_key_key (fk_bios_config_component_id ASC, settings_key ASC),
+    CONSTRAINT bios_config_settings_fk_bios_config_component_id_fkey FOREIGN KEY (fk_bios_config_component_id) REFERENCES public.bios_config_components(id) ON DELETE CASCADE
+);
+
+CREATE TABLE public.event_history (
+    event_id UUID NOT NULL,
+    event_type STRING NOT NULL,
+    event_start TIMESTAMPTZ NOT NULL,
+    event_end TIMESTAMPTZ NOT NULL,
+    target_server UUID NOT NULL,
+    parameters JSONB NULL,
+    final_state STRING NOT NULL,
+    final_status JSONB NULL,
+    CONSTRAINT event_history_pkey PRIMARY KEY (event_id ASC, event_type ASC, target_server ASC),
+    CONSTRAINT event_history_target_server_fkey FOREIGN KEY (target_server) REFERENCES public.servers(id) ON DELETE CASCADE
+);
+
+CREATE TABLE public.firmware_set_validation_facts (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    firmware_set_id UUID NOT NULL,
+    target_server_id UUID NOT NULL,
+    performed_on TIMESTAMPTZ NOT NULL,
+    CONSTRAINT firmware_set_validation_facts_pkey PRIMARY KEY (id ASC),
+    CONSTRAINT firmware_set_validation_facts_firmware_set_id_fkey FOREIGN KEY (firmware_set_id) REFERENCES public.component_firmware_set(id) ON DELETE CASCADE
+);
 -- +goose StatementEnd
+
+
 -- +goose Down
+-- +goose StatementBegin
+DROP TABLE public.firmware_set_validation_facts;
+DROP TABLE public.event_history;
+DROP TABLE public.bios_config_settings;
+DROP TABLE public.bios_config_components;
+DROP TABLE public.bios_config_sets;
+DROP TABLE public.bmc_mac_address;
+DROP TABLE public.aoc_mac_address;
+DROP TABLE public.bom_info;
+DROP TABLE public.attributes_firmware_set;
+DROP TABLE public.component_firmware_set_map;
+DROP TABLE public.component_firmware_set;
+DROP TABLE public.server_credentials;
+DROP TABLE public.server_credential_types;
+DROP TABLE public.component_firmware_version;
+DROP TABLE public.attributes;
+DROP TABLE public.versioned_attributes;
+DROP TABLE public.server_components;
+DROP TABLE public.server_component_types;
+DROP TABLE public.servers;
+-- +goose StatementEnd
--- db/migrations/00003_add_install_inband_column.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE public.component_firmware_version ADD COLUMN install_inband BOOL NOT NULL DEFAULT false;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE public.component_firmware_version DROP COLUMN install_inband;
--- +goose StatementEnd
